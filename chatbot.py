dd760e70b80c8d413476f1f12883c40525c98c0e
LeilaKarimii <131779385+LeilaKarimii@users.noreply.github.com>
2023-07-27 19:27:27 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,6 @@ import User_Ursula as ursula
 import hashlib
 from datetime import datetime
 
-
 # Load the data
 @st.cache_data
 def data_load():
@@ -33,36 +32,56 @@ def chatbot():
     st.title("Game Recommendation Chatbot")
     st.write("Welcome! Let's start chatting.")
 
-    user_name = st.text_input("Please enter your name:")
+    chat_history = []
+    loopy = 0
 
-    if user_name.strip():  # Check if user_name is not empty or only whitespace
-        user_ids = get_user_ids(user_name)
+    # Chat loop
+    while True:
+        loopy += 1
+        key_a = get_unique_key(f'blabla-{loopy}')
+        key_b = get_unique_key(f'boob-{loopy}')
+        user_name = st.text_input("Please enter your name:", key=key_a)
 
-        if len(user_ids) == 0:
-            # User name not found in the data
-            robot_response = f"Hello, {user_name}! I couldn't find any user ID associated with your name. Please provide me with your user ID so I can assist you better."
-        elif len(user_ids) == 1:
-            # Only one user ID found
-            user_id = user_ids[0]
-            robot_response = f"Hello, {user_name}! How can I assist you with Game recommendations?"
-        else:
-            # Multiple user IDs found
-            user_id_input = st.text_input("Multiple user IDs found. Please enter your preferred user ID:")
+        if user_name.strip():  # Check if user_name is not empty or only whitespace
+            user_ids = get_user_ids(user_name)
 
-            if user_id_input:
-                try:
-                    user_id_input = int(user_id_input)
-                    if user_id_input in user_ids:
-                        robot_response = f"Hello, {user_name}! How can I assist you with Game recommendations?"
-                    else:
-                        robot_response = f"Sorry, {user_name}! The provided user ID does not match any of the user IDs associated with your name. Please enter your user ID again."
-                except ValueError:
-                    robot_response = "Please enter a valid numeric user ID."
+            if len(user_ids) == 0:
+                # User name not found in the data
+                robot_response = f"Hello, {user_name}! I couldn't find any user ID associated with your name. Please provide me with your user ID so I can assist you better."
+            elif len(user_ids) == 1:
+                # Only one user ID found
+                user_id = user_ids[0]
+                robot_response = f"Hello, {user_name}! How can I assist you with Game recommendations?"
             else:
-                return
+                # Multiple user IDs found
+                user_id_input = st.text_input("Multiple user IDs found. Please enter your preferred user ID:", key=key_b)
 
-        # Display the robot response
-        st.text_area("Robot:", value=robot_response, key="robot-response", disabled=True)
+                if user_id_input:
+                    try:
+                        user_id_input = int(user_id_input)
+                        if user_id_input in user_ids:
+                            robot_response = f"Hello, {user_name}! How can I assist you with Game recommendations?"
+                        else:
+                            robot_response = f"Sorry, {user_name}! The provided user ID does not match any of the user IDs associated with your name. Please enter your user ID again."
+                    except ValueError:
+                        robot_response = "Please enter a valid numeric user ID."
+                else:
+                    continue
+
+            # Add user input to chat history
+            chat_history.append(("User", user_name))
+            # Add robot response to chat history
+            chat_history.append(("Robot", robot_response))
+
+            # Display the last robot response
+            if chat_history:
+                last_sender, last_message = chat_history[-1]
+                if last_sender == "Robot":
+                    st.text_area("Robot:", value=last_message, key="robot-response", disabled=True)
+
+        # Break the chat loop if user input is "quit"
+        if user_name.lower() == "quit":
+            break
 
 if __name__ == "__main__":
     chatbot()
