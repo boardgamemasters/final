f3573d3eb1943b8db74aa74c6082f7213ae50e1e
LeilaKarimii <131779385+LeilaKarimii@users.noreply.github.com>
2023-07-28 16:12:51 +0200

    Update llchat.py

--- llchat.py
+++ llchat.py
@@ -1,32 +1,29 @@
 import streamlit as st
 import pandas as pd
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.metrics.pairwise import cosine_similarity
+import ameyfun as amey
+from streamlit_chat import message
 
 # Load the data
 @st.cache_data
 def data_load():
-    rating_df   =    pd.read_csv('data/final_ratings_v3.csv')
-    games_df    =    pd.read_csv('data/game_learn_df_v3.csv')
-    users_df    =    pd.read_csv('data/usernames_v2.csv')
-    games_info  =    pd.read_csv('data/bgref.csv')
-    cosine_df   =    pd.read_csv('data/bg_cosines_final.csv')
-    return rating_df, games_df, users_df, games_info, cosine_df
+    rating_df = pd.read_csv('data/final_ratings_v3.csv')
+    games_df = pd.read_csv('data/game_learn_df_v3.csv')
+    users_df = pd.read_csv('data/usernames_v2.csv')
+    games_info = pd.read_csv('data/bgref.csv')
+    cosine_df = pd.read_csv('data/bg_cosines_final.csv')
+    final_df = pd.read_csv('data/bg_final_data.csv')
+    return rating_df, games_df, users_df, games_info, cosine_df, final_df
 
-rating_df, games_df, users_df, games_info, cosine_df = data_load()
 
-# Function to check if user exists
-def get_user_ids(user_name):
-    user_ids = rating_df.loc[rating_df['Username'] == user_name, 'user_name'].values
-    return user_ids
+rating_df, games_df, users_df, games_info, cosine_df, final_df = data_load()
 
 # Function to find the correct category of the favorite game
 def find_right_category(favorite_game, data):
     for i, row in data.iterrows():
-        if favorite_game.lower() in row['name_lower']:
+        if favorite_game.lower() in row['game_name_lower']:
             return row['consolidated_category_name']
 
-def game_of_my_life(user_favorite_game, data, z=6):
+def game_recommendation(user_favorite_game, data, z=6):
     # Find the right category of the favorite game
     right_category_info = find_right_category(user_favorite_game, data)
 
@@ -35,7 +32,7 @@ def game_of_my_life(user_favorite_game, data, z=6):
 
     # Find games with at least one shared category from the favorite game's categories
     similar_games_with_shared_category = data[data['consolidated_category_name'].apply(lambda x: any(cat in x for cat in favorite_game_categories))]
-    similar_games_with_shared_category = similar_games_with_shared_category[similar_games_with_shared_category['name_x'] != user_favorite_game]
+    similar_games_with_shared_category = similar_games_with_shared_category[similar_games_with_shared_category['game_name'] != user_favorite_game]
 
     # Convert consolidated_category_name to list of strings
     similar_games_with_shared_category['consolidated_category_name'] = similar_games_with_shared_category['consolidated_category_name'].apply(eval)
@@ -74,12 +71,12 @@ def chatbot():
         key_b = f'boob{loopy}'
 
         # Ask for user's favorite game directly
-        user_favorite_game = st.text_input("Please enter your favorite game:", key=key_a)
+        user_favorite_game = st.text_input("Please enter the name of the game that you like:", key=key_a)
 
         if user_favorite_game.strip():  # Check if the favorite game is not empty or only whitespace
             # Recommend games based on the user's favorite game
-            recommended_game_ids = game_of_my_life(user_favorite_game, games_df)
-            recommended_games = games_df[games_df['bgg_id'].isin(recommended_game_ids)]['name_x'].tolist()
+            recommended_game_ids = game_recommendation(user_favorite_game, final_df)
+            recommended_games = final_df[final_df['bgg_id'].isin(recommended_game_ids)]['game_name'].tolist()
 
             # Add user input to chat history
             chat_history.append(("User", user_favorite_game))
@@ -98,4 +95,5 @@ def chatbot():
 
 if __name__ == "__main__":
     chatbot()
+
   
