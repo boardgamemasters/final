8898c23464c6b15b7b0cdbc34f18e001517f6700
LeilaKarimii <131779385+LeilaKarimii@users.noreply.github.com>
2023-07-28 17:07:30 +0200

    Update llchat.py

--- llchat.py
+++ llchat.py
@@ -1,25 +1,18 @@
 import streamlit as st
 import pandas as pd
-import ameyfun as amey
+from ameyfun import game_of_my_life
 
 # Load the data
-@st.cache(allow_output_mutation=True)
+@st.cache
 def data_load():
-    final_df = pd.read_csv('data/final_data.csv')
-    return final_df
+    final_data = pd.read_csv('final_data.csv')
+    return final_data
 
-final_df = data_load()
+final_data = data_load()
 
-# Function to check if game exists
-def get_game_ids(game_name):
-    game_ids = final_df.loc[final_df['Name'] == game_name, 'Name'].values
-    return game_ids
-
-# Function to recommend games based on user input
-def recommend_games(user_favorite_game, data, z=6):
-    # Use the ameyfun function to get game recommendations
-    similar_game_bgg_ids = amey.game_of_my_life(user_favorite_game, data, z)
-    return similar_game_bgg_ids
+# Emoji characters for robot and user
+robot_emoji = "🤖"
+user_emoji = "👤"
 
 # Chatbot function
 def chatbot():
@@ -29,28 +22,21 @@ def chatbot():
     chat_history = []
 
     # Chat loop
-    loopy = 0
     while True:
-        loopy += 1
-        key_a = f'blabla{loopy}'
-        key_b = f'boob{loopy}'
-        user_name = st.text_input("Please enter the Name of Game that you like:", key=key_a)
+        user_favorite_game = st.text_input(f"{user_emoji} Please enter the name of the game that you like:")
 
-        if user_name.strip():  # Check if user_name is not empty or only whitespace
-            game_ids = get_game_ids(user_name)
+        if user_favorite_game.strip():  # Check if user input is not empty or only whitespace
+            game_recommendations = game_of_my_life(user_favorite_game, final_data)
 
-            if len(game_ids) == 0:
-                # Game name not found in the data
-                robot_response = f"🤖 Hello, {user_name}! I couldn't find any game associated with your input. Please provide me with another game name."
-            elif len(game_ids) == 1:
-                # Only one game found
-                robot_response = f"🤖 Hello, {user_name}! How can I assist you with game recommendations?"
+            if not game_recommendations:
+                robot_response = f"{robot_emoji} Sorry, I couldn't find any game recommendations for '{user_favorite_game}'. Please try again with a different game name."
             else:
-                # Multiple games found
-                robot_response = f"🤖 Hello, {user_name}! I found multiple games associated with your input. Please enter a more specific game name."
+                robot_response = f"{robot_emoji} Sure! Based on '{user_favorite_game}', I recommend the following games:\n"
+                for i, game in enumerate(game_recommendations, 1):
+                    robot_response += f"{i}. {game}\n"
 
             # Add user input to chat history
-            chat_history.append(("User", user_name))
+            chat_history.append(("User", user_favorite_game))
             # Add robot response to chat history
             chat_history.append(("Robot", robot_response))
 
@@ -58,17 +44,10 @@ def chatbot():
             if chat_history:
                 last_sender, last_message = chat_history[-1]
                 if last_sender == "Robot":
-                    st.text_area("🤖 Robot:", value=last_message, key="robot-response", disabled=True)
-
-        # Game recommendation
-        user_favorite_game = st.text_input("👤 Please enter the name of the game that you like:", key=key_b)
-        if user_favorite_game.strip():
-            similar_game_bgg_ids = recommend_games(user_favorite_game, final_df)
-            st.write("Recommended Games:")
-            st.write(similar_game_bgg_ids)
+                    st.text_area(f"{robot_emoji} Robot:", value=last_message, key="robot-response", disabled=True)
 
         # Break the chat loop if user input is "quit"
-        if user_name.lower() == "quit":
+        if user_favorite_game.lower() == "quit":
             break
 
 if __name__ == "__main__":
