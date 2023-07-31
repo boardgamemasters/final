29fdcfed464539fe7ec0634848125f920f360e4e
Frederik Loy <loy.frederik@gmail.com>
2023-07-31 20:37:26 +0200

    aasdfasdf

--- steamlit.py
+++ steamlit.py
@@ -249,7 +249,7 @@ elif rec_select == 'Chatbot Recommender':
                        sel_game = games[games.str.fullmatch(response, case = False)][0].item()     
                     else:
                      sel_game = games[games.str.fullmatch(response, case = False)].item()     
-                    st.write(sel_game)
+                    # st.write(sel_game)
                     selecthor = 1
                     message(st.session_state.questions[2], key=f"b2{count}")  
                     continue
