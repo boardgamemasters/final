b051ca94f29e13482be07cdb0107ca712893f060
Frederik Loy <loy.frederik@gmail.com>
2023-07-31 16:45:04 +0200

    asdfasdf

--- playground.py
+++ playground.py
@@ -178,20 +178,11 @@ elif rec_select == 'Similar Taste':
     # st.sidebar.text('Login to use this Feature')    # (pop_movies.iloc[i+2]['title'])
     user_games = ursula.gib_spiele_digga(rat_df = rating_df, s_alt = user_feature['amount'], user = user_feature['user_id'],game_frame=games_df)
     user_games = ursula.get_feature(result_file=user_games, feature_file=games_info)
-    # user_col = len(user_games)
-    # u_cols = st.columns(user_col)
-    # with st.container():
-    #     st.header(f'Special Treats for you {user_feature["name"]}')
-    #     for i, x in enumerate(u_cols):
-    # #         st.header(user_games.iloc[i]['title'])
-    #         # st.image(user_games.iloc[i]['img'])
-    #         st.header(user_games.iloc[i]['bgg_id'])
-    #         st.text(user_games.iloc[i]['predicted_rating'])
     ncol = len(user_games)
     with st.container():
         st.header(f'Special Treats for you {user_feature["name"]}')
-        for i in range(0, ncol, 3):
-            col1, col2, col3 = st.columns(3)
+        for i in range(0, ncol, 5):
+            col1, col2, col3, col4, col5 = st.columns(5)
             with col1:
                 st.image(user_games.iloc[i]['image'])
                 st.text(user_games.iloc[i]['name'])
@@ -203,6 +194,15 @@ elif rec_select == 'Similar Taste':
                 if i + 2 < ncol:
                     st.image(user_games.iloc[i+2]['image'])
                     st.text(user_games.iloc[i+2]['name'])
+            with col4:                 
+                if i + 3 < ncol:
+                    st.image(user_games.iloc[i+3]['image'])
+                    st.text(user_games.iloc[i+3]['name'])
+            with col5:                 
+                if i + 4 < ncol:
+                    st.image(user_games.iloc[i+4]['image'])
+                    st.text(user_games.iloc[i+4]['name'])
+            
 
 elif rec_select == 'Amey likes you a lot':  
     def amey_like():
@@ -358,3 +358,34 @@ elif rec_select == 'Chatbot Recommender':
 
 else:
     st.write('')
+
+if st.session_state['user_login']==True:
+
+    user_games = ursula.gib_spiele_digga(rat_df = rating_df, s_alt = 10, user = st.session_state['user_name'], game_frame=games_df)
+    user_games = ursula.get_feature(result_file=user_games, feature_file=games_info)
+    ncol = len(user_games)
+    with st.container():
+        st.header(f'Special Treats for you {user_feature["name"]}')
+        for i in range(0, ncol, 5):
+            col1, col2, col3, col4, col5 = st.columns(5)
+            with col1:
+                st.image(user_games.iloc[i]['image'])
+                st.text(user_games.iloc[i]['name'])
+            with col2:
+                if i + 1 < ncol:
+                    st.image(user_games.iloc[i+1]['image'])
+                    st.text(user_games.iloc[i+1]['name'])                    
+            with col3:                 
+                if i + 2 < ncol:
+                    st.image(user_games.iloc[i+2]['image'])
+                    st.text(user_games.iloc[i+2]['name'])
+            with col4:                 
+                if i + 3 < ncol:
+                    st.image(user_games.iloc[i+3]['image'])
+                    st.text(user_games.iloc[i+3]['name'])
+            with col5:                 
+                if i + 4 < ncol:
+                    st.image(user_games.iloc[i+4]['image'])
+                    st.text(user_games.iloc[i+4]['name'])
+else:
+    st.write('') 
