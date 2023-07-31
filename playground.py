878bba2a15386a739f81f6dcef2495adc7f21a65
Frederik Loy <loy.frederik@gmail.com>
2023-07-31 18:34:41 +0200

    asdf

--- playground.py
+++ playground.py
@@ -242,7 +242,7 @@ elif rec_select == 'Amey likes you a lot':
 
 
 elif rec_select == 'Chatbot Recommender':
-    chat_col = st.columns([.5])
+    chat_col = st.columns()
     games = amey_df['name_x']
     # st.sidebar.text('Coming Soon')
     def on_input_change():
